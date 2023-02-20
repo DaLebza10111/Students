334b4c7ecd430643adfa89c221a34c9b68420979
Lebohang Sopazi <lebohang.sopazi@firsttech.digital>
2023-02-20 21:46:18 +0200

    Implement the IStudentManagerRepository interface

---
+++ API/IRepository/IStudentManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Models;
+
+namespace API.IRepository
+{
+    public interface IStudentManagerRepository
+    {
+        Task<IEnumerable<StudentModelModel>> GetStudents();
+        Task<StudentModelModel?> GetStudent(int StudentModelModelId);
+        Task<StudentModelModel> AddStudent(StudentModelModel student);
+        Task<StudentModelModel?> UpdateStudent(StudentModelModel student);
+        Task DeleteStudentAsync(int StudentModelModelId);
+    }
+}
