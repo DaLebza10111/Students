2f98c206ba6dad986fff74bac3b2ef2dba362c66
Lebohang Sopazi <lebohang.sopazi@firsttech.digital>
2023-02-20 21:15:20 +0200

    Add reference to the connection string

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,14 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
