df18879ae9930e6a076f04291ec7b1e49164e22a
Lebohang Sopazi <lebohang.sopazi@firsttech.digital>
2023-02-20 13:40:56 +0200

    Add Dbcontext class

---
+++ API/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace API
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+        public DbSet<StudentModelModel> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
