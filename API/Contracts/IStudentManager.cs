75c364919e7fda14f84677d58497057786cdbeaa
Lebohang Sopazi <lebohang.sopazi@firsttech.digital>
2023-02-20 21:30:31 +0200

    Add interface for the repository pattern

---
+++ API/Contracts/IStudentManager.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Models;
+
+namespace API.Contracts
+{
+    public interface IStudentManager
+    {
+        Task<IEnumerable<StudentModelModel>> GetEmployees();
+        Task<StudentModelModel> GetEmployee(int StudentModelModelId);
+        Task<StudentModelModel> AddEmployee(StudentModelModel student);
+        Task<StudentModelModel> UpdateEmployee(StudentModelModel student);
+        void DeleteEmployee(int StudentModelModelId);
+    }
+}
