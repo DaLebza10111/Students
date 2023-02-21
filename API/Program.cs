6c9bf7ff23380e8a0964078694f706e9f4b4c3fd
Lebohang Sopazi <lebohang.sopazi@firsttech.digital>
2023-02-21 07:33:19 +0200

    Register the repositories

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API;
+using API.IRepository;
+using API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Configuration;
@@ -9,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+builder.Services.AddScoped<IStudentManagerRepository, StudentManagerRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
